baf2631a1d072ef50df3657a2a5d084e11063955
mikepilar <mike_pilar@hotmail.com>
2026-04-03 10:26:44 -0400

    Add docstring to __init__.py
    Added module docstring with author and description.

--- planprint/__init__.py
+++ planprint/__init__.py
@@ -1,1 +1,11 @@
+# -----------------------------------------------------------------------------
+# planprint
+# __init__.py
+#
+# Author:      Mike Pilar
+# Created:     2026-04-03
+# Description: Package initializer. Exposes the primary public interface for 
+#              the planprint package.
+# -----------------------------------------------------------------------------
+
 from .fingerprint import get_full_fingerprint, generate_sweep_fingerprint, generate_refine_fingerprint
